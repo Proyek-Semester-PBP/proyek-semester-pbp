3b535ca834e8a96e6a2f88590060a89c263a610f
ferupk <feru.pratama@ui.ac.id>
2022-10-27 15:14:47 +0700

    push to branch test, updated shopping module

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class RecommendedItem(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.BigIntegerField()
+    description = models.TextField()
+    link = models.URLField()
+
+class RecommendedVendor(models.Model):
+    name = models.CharField(max_length=150)
+    description = description = models.TextField()
+    link = models.URLField()
--- shopping/urls.py
+++ shopping/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from shopping.views import show_shop
+from shopping.views import show_shop, show_item_json, show_vendor_json
 
 app_name = 'shopping'
 
 urlpatterns = [
     path('', show_shop, name='show_shop'),
+    path('json-item/', show_item_json, name='show_item_json'),
+    path('json-vendor/', show_vendor_json, name='show_vendor_json'),
 ]
