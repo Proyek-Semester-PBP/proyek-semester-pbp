6e0a374d5ac08abc056ee50f1ea227ed52524f00
ferupk <feru.pratama@ui.ac.id>
2022-10-29 21:46:30 +0700

    implemented product and vendor recommendation

--- shopping/models.py
+++ shopping/models.py
@@ -1,13 +1,17 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 class RecommendedItem(models.Model):
     name = models.CharField(max_length=150)
+    image = models.CharField(max_length=255, default="/static/shopping/items/placeholder_item.jpg")
     price = models.BigIntegerField()
     description = models.TextField()
     link = models.URLField()
 
 class RecommendedVendor(models.Model):
     name = models.CharField(max_length=150)
-    description = description = models.TextField()
+    image = models.CharField(max_length=255, default="/static/shopping/vendors/placeholder_vendor.jpg")
+    description = models.TextField()
     link = models.URLField()
--- shopping/views.py
+++ shopping/views.py
@@ -2,28 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from shopping.models import RecommendedItem, RecommendedVendor
-from random import randint
 
 # Create your views here.
 def show_shop(request):
     return render(request, "shop.html")
 
 def show_item_json(request):
-    itemAmount = RecommendedItem.objects.all().count()
-    itemData = RecommendedItem.objects.filter(pk__in=generate_pk(6, itemAmount))
+    itemData = RecommendedItem.objects.order_by('?')[:6]
     return HttpResponse(serializers.serialize("json", itemData), content_type="application/json")
 
 def show_vendor_json(request):
-    vendorAmount = RecommendedVendor.objects.all().count()
-    vendorData = RecommendedVendor.objects.filter(pk__in=generate_pk(3, vendorAmount))
+    vendorData = RecommendedVendor.objects.order_by('?')[:3]
     return HttpResponse(serializers.serialize("json", vendorData), content_type="application/json")
-
-def generate_pk(amount, max):
-    pk_list = []
-
-    while len(pk_list) != amount:
-        pk = randint(1,max)
-        if pk not in pk_list:
-            pk_list.append(pk)
-
-    return pk_list
