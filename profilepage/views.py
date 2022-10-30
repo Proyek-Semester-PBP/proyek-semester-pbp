b77c5bf3760ed579122c0d779fce3c7db419d723
alvaro.luqman1 <alvaro.luqman@gmail.com>
2022-10-30 09:59:23 +0700

    Update Profile

--- profilepage/views.py
+++ profilepage/views.py
@@ -1,4 +1,5 @@
 from asyncio.log import logger
+import http
 from http.client import HTTPResponse
 from multiprocessing import context
 from django.shortcuts import render
@@ -9,18 +10,8 @@ from home.models import User
 # Create your views here.
 def show_profile(request):
     user = request.user
-    data_user1 = Profile.objects.get(user = user)
-    context = {
-        'user': request.user,
-        'name': data_user1.name,
-        'email': data_user1.email,
-        'mobile': data_user1.mobile,
-        'github': data_user1.github,
-        'instagram': data_user1.instagram,
-        'twitter': data_user1.twitter,
-        'facebook': data_user1.facebook,
-    }
-    if request.method == "POST":
+    
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         name = request.POST.get("name")
         email = request.POST.get("email")
         mobile = request.POST.get("mobile")
@@ -28,18 +19,23 @@ def show_profile(request):
         instagram = request.POST.get("instagram")
         twitter = request.POST.get("twitter")
         facebook = request.POST.get("facebook")
+        Profile.objects.filter(user = user).update(name = name, email = email, mobile = mobile, github = github, instagram = instagram, twitter = twitter, facebook = facebook)
     
-        i = Profile.objects.filter(user = user).update(name = name, email = email, mobile = mobile, github = github, instagram = instagram, twitter = twitter, facebook = facebook)
+    data_user1 = Profile.objects.get(user = user)
+    context = {
+        'user': user,
+        'name': data_user1.name,
+        'email': data_user1.email,
+        'mobile': data_user1.mobile,
+        'github': data_user1.github,
+        'instagram': data_user1.instagram,
+        'twitter': data_user1.twitter,
+        'facebook': data_user1.facebook,
+        'point': data_user1.point,
+        'plastic': data_user1.plastics
+    }
+    return render(request, "profile.html", context)
     
-        return render(request, "profile.html", context)
-    else:
-        return render(request, "profile.html", context)
-
-    
+       
 
 
-def editProfile(user, name, email, mobile, github, instagram, twitter, facebook):
-    return Profile.objects.create(user=user, name=name, email=email, mobile=mobile, github=github, instagram=instagram, twitter=twitter, facebook=facebook )
-
-def is_ajax(request):
-    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
