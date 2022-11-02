daf2a8b281a978c7a654c920efd16026092d4bcc
AlvaroLuqmanW <99391051+AlvaroLuqmanW@users.noreply.github.com>
2022-11-03 00:41:03 +0700

    Update models.py

--- profilepage/models.py
+++ profilepage/models.py
@@ -2,7 +2,7 @@ from distutils.command.upload import upload
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User 
-from tkinter import CASCADE
+
 
 
 # Create your models here.
