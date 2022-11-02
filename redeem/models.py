c7b12c3223547c76e07e4167afd7cf861d8e9f13
Syifa Cahya <syfcahyakamila@gmail.com>
2022-11-02 15:54:41 +0700

    progress

--- redeem/models.py
+++ redeem/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Voucher(models.Model):
     jenis_voucher = models.TextField()
     title_voucher =  models.TextField()
-    produk_voucher = models.TextField()
     harga_poin = models.IntegerField()
     deskripsi = models.TextField()
     
--- redeem/views.py
+++ redeem/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from redeem.models import Voucher
+from django.core import serializers
+from django.http.response import JsonResponse, HttpResponse
 
 # Create your views here.
 def show_redeem(request):
     return render(request, "redeem.html")
+
+def show_json(request):
+    voucher = Voucher.objects.all()
+    return HttpResponse(serializers.serialize('json', voucher), content_type='application/json')
