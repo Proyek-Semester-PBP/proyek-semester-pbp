0547bc1801b68f5e9587bfa0c086e9e60acd9913
alvaro.luqman1 <alvaro.luqman@gmail.com>
2022-12-12 16:14:25 +0700

    Edit

--- authentication/views.py
+++ authentication/views.py
@@ -39,6 +39,7 @@ def login(request):
 def NewProfile(user):
     try:
         temp = Profile.objects.get(user = user) 
+        return temp
     except Profile.DoesNotExist:
         url = "https://img.freepik.com/free-vector/cute-cow-surprised-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3874.jpg?w=360"
         RecycleHistory.objects.create(user = user, name = user, date = "-", weight = 0, point = 0, location = "-", is_pickup = False, description = "-")
